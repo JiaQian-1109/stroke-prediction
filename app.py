2dd42060d255c760de6eb6c6ca302612e777b879
JiaQian-1109 <jqwong031109@gmail.com>
2025-05-04 01:23:02 +0800

    Add JiaQian and YiXin image files

--- app.py
+++ app.py
@@ -557,8 +557,9 @@ elif st.session_state.page == "About Us":
                 <br><br><br>
                 <img src="https://raw.githubusercontent.com/JiaQian-1109/stroke-prediction/main/JiaQian.jpeg" alt="jiaqian" width="200" height="200" style="margin-left: 15%; margin-right: 30px; text-align: center;">
                 <img src="https://raw.githubusercontent.com/JiaQian-1109/stroke-prediction/main/YiXin.jpeg" alt="yixin" width="200" height="200" style="margin-left: 30px; text-align: center;">
-                <br><br>
+                <br>
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Wong Jia Qian&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Soh Yi Xin
+                <br><br>
     </div>
     """, unsafe_allow_html=True)
 
